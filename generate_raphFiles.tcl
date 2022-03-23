5095bfff1d6e18e16178a574d371dc479c263b2b
Ricardo Hernandez <ricardh@sakuya.ecen.okstate.edu>
2022-03-23 16:07:26 -0500

    Okita Files

---
+++ generate_raphFiles.tcl
@@ -0,0 +1,5 @@
+read_process -file "processfile" -name processfile
+
+metal_rules_gen -file "../data/ext_custom.rules" -dir "raphFiles3D" -name patterns -pattern 6 -write_to_solver -keep_file
+
+exit
